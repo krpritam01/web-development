6c5524b1b043f8b4d37375c257b12e71924481dd
PRAKASH KUMAR PRITAM <125028639+krpritam01@users.noreply.github.com>
2023-08-16 07:16:06 -0700

    Add files via upload

---
+++ week-13-JavaScript-Fundamentals-1/JS-fundamental-1-Assi/prob3.js
@@ -0,0 +1,6 @@
+// You are working on a currency conversion application. Write a program that has a variable which stores the amount in Indian Rupees (INR) and prints the equivalent amount in US Dollars (USD). Use the current exchange rate of 1 USD = 82 INR.
+
+let ammountInINR = 920;
+let ammountInUSD = 920 / 82;
+console.log(`${ammountInINR} INR is ${ammountInUSD} USD`);
+
